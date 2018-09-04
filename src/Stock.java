dbb68a6abcfccb7144b77b862f66f1f78e67824f
Ana Paula Montenegro <38986710+MontenegroAni@users.noreply.github.com>
2018-09-03 22:15:46 -0300

    Add files via upload

---
+++ src/Stock.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Stock {
+	private List<ItemStock> items = new ArrayList<ItemStock>();
+	
+	public List<ItemStock> getItems(){
+		return this.items;
+		}
+
+	public void setItems(List<ItemStock> newItems){
+		this.items.add(newItems);
+		}
+
+	public List<ItemStock> consultarItemsFaltantes(int cantidadMaxima){
+		List<ItemStock> resultado= new ArrayList<ItemStock>();
+			for(ItemStock is: items){
+				if(cantidadMaxima<is.getCantidad()){
+					resultado.add(is);
+				}
+			}
+		return resultado;
+		}
+
+
+}
